1ef0a20c584a1fb35a6148aa28835570f2c2ce7b
Xinyi Zhong <zhongxy14@gmail.com>
2020-07-22 22:20:19 -0400

    I don't know what am I doing. This is correct

--- AMP_free.py
+++ AMP_free.py
@@ -1,28 +1,9 @@
 # Implementation of AMP algorithm for symmetric +/- 1 priors
-# and rotationally invariant noise
+# and square symmetric, rotationally invariant noise
 
 import numpy as np
-import math
 from numpy.polynomial import polynomial
-
-import matplotlib.pyplot as plt
-# from AMP_gg import plot_save
-
-def pdf_mu_sigmasq(x, mu,sigmasq):
-    return 1/(2*np.sqrt(2*np.pi*sigmasq)) * np.exp(-(x-mu)**2/(2*sigmasq)) + 1/(2*np.sqrt(2*np.pi*sigmasq)) * np.exp(-(x+mu)**2/(2*sigmasq)) 
-
-def plot_save(f, mu, sigmasq, t):
-    fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (7*1, 5))
-    ax.hist(f, bins = 50, density = True)
-    start = np.quantile(f, 0.05)*1.5
-    end = np.quantile(f,0.95)*1.5
-    xgrid = np.linspace(start, end, 50)
-    ax.plot(xgrid, pdf_mu_sigmasq(xgrid, mu, sigmasq), color = 'orange')
-    ax.vlines(x = mu, ymin = 0, ymax = pdf_mu_sigmasq(mu, mu, sigmasq), color = 'orange')
-    ax.vlines(x = -mu, ymin = 0, ymax = pdf_mu_sigmasq(-mu, mu, sigmasq), color = 'orange')     
-    ax.set_title("at_iter {}".format(t))
-    fig.savefig("figures/Free_at_iter_{}.png".format(t))
-    plt.close()
+from debug import plot_save
 
 # Compute first K free cumulants from moments
 #
@@ -40,8 +21,8 @@ def cumulants_from_moments(m,K):
 
 # Remove top eigenvalues, and compute moments of remaining eigenvalue
 # distribution
-def compute_moments(Y,k,remove=1):
-  eigs = np.linalg.eigvalsh(Y)[:-remove]
+def compute_moments(X,k,remove=1):
+  eigs = np.linalg.eigvalsh(X)[:-remove]
   m = []
   for j in range(k+1):
     m.append(np.mean(eigs**j))
@@ -80,7 +61,6 @@ def compute_b(Phi,t,kappa):
 #           [ sigma_{21}, ... , sigma_{2t} ],
 #             ...
 #           [ sigma_{t1}, ... , sigma_{tt} ]]
-
 def compute_Sigma(Delta,Phi,t,kappa):
   Sigma = np.zeros(Delta.shape)
   for j in range(2*t-1):
@@ -94,7 +74,8 @@ def compute_Sigma(Delta,Phi,t,kappa):
 
 # AMP with free cumulant corrections
 #
-# Inputs: u = u_1, the initialization
+# Inputs: X, the data matrix
+#         u = u_1, the initialization
 #         innerprod = <u_1 u_*>
 #
 # For symmetric +/- 1 prior, the posterior mean denoiser E[U_* | F] when
@@ -104,23 +85,23 @@ def compute_Sigma(Delta,Phi,t,kappa):
 # Its gradient is
 #   grad u(F) = Sigma^{-1} mu * [1 - u(F)^2]
 #
-def AMP_free(Y,s,u,innerprod,iters=10,rank=1,reg=0.01):
-  n = Y.shape[0]
-  moments = compute_moments(Y,2*iters,remove=rank)
+def AMP_free(X,u,innerprod,iters=10,rank=1,reg=0.001,plot_prefix=None):
+  # Compute moments and cumulants of noise
+  n = X.shape[0]
+  moments = compute_moments(X,2*iters,remove=rank)
   kappa = cumulants_from_moments(moments,2*iters)
+  # Initialize U, F, mu, Delta, Phi
   U = np.reshape(u,(-1,1))
+  F = np.zeros(shape=(n,0))
+  mu = np.array([])
   Delta = np.zeros((iters+1,iters+1))
   Phi = np.zeros((iters+1,iters+1))
   Delta[0,0] = np.mean(u**2)
-  mu = np.array([])
   for t in range(1,iters+1):
     # Compute f_t
     b = compute_b(Phi,t,kappa)
-    f = Y.dot(U[:,-1])-U.dot(b)
-    if t == 1:
-      F = np.reshape(f,(-1,1))
-    else:
-      F = np.hstack((F,np.reshape(f,(-1,1))))
+    f = X.dot(U[:,-1])-U.dot(b)
+    F = np.hstack((F,np.reshape(f,(-1,1))))
     # Update Sigma
     #   (For numerical stability, ensure lambda_min(Sigma) >= reg)
     Sigma = compute_Sigma(Delta,Phi,t,kappa)
@@ -129,20 +110,20 @@ def AMP_free(Y,s,u,innerprod,iters=10,rank=1,reg=0.01):
     Sigma = Q.dot(np.diag(D)).dot(np.transpose(Q))
     # Update mu empirically using E[F_t^2] = mu_t^2 + Sigma_{t,t}
     #   (For numerical stability, ensure mu_t >= reg)
-    mu_t = np.sqrt(max(np.mean(f**2)-Sigma[t-1,t-1], reg))
+    mu_t = np.sqrt(max(np.mean(f**2)-Sigma[t-1,t-1],reg**2))
     mu = np.append(mu,mu_t)
-    # sanity check
-    plot_save(f,mu[-1],Sigma[-1,-1], t)
     # Compute u_{t+1}
     Sinvmu = np.linalg.solve(Sigma,mu)
     u = np.tanh(F.dot(Sinvmu))
     U = np.hstack((U,np.reshape(u,(-1,1))))
+    # Plot empirical distribution, for debugging
+    if plot_prefix is not None:
+      filename = '%s_iter%02d.png' % (plot_prefix,t)
+      plot_save(F.dot(Sinvmu), mu.dot(Sinvmu), mu.dot(Sinvmu), filename)
     # Update Delta empirically
-    Euprod = np.transpose(U).dot(u)/n
-    Delta[t,:(t+1)] = Euprod
-    Delta[:(t+1),t] = Euprod
+    Delta[:(t+1),:(t+1)] = np.transpose(U).dot(U)/n
     # Update Phi empirically
-    gradu = Sinvmu * (1-Euprod[t])
+    gradu = Sinvmu * (1-Delta[t,t])
     Phi[t,:t] = gradu
   return U
 
--- test_free.py
+++ test_free.py
@@ -2,61 +2,58 @@
 
 import numpy as np
 from AMP_free import AMP_free
+from debug import hist_save
 
-np.random.seed(3872)
+np.random.seed(123)
 
-n = 1000
-s = 1.3
-innerprod = 0.9
-sigma = 1-innerprod**2
+ntrials = 5
+niters = 10
 
-'''
-# Gaussian noise
-print('Gaussian noise')
-for i in range(1):
-  W = np.random.normal(size=(n,n))
-  W = (W+np.transpose(W))/np.sqrt(2*n)
+n = 3000
+innerprod = 0.2
+sigma = np.sqrt(1-innerprod**2)
+
+def test(W,prefix):
   ustar = np.random.binomial(1,0.5,size=n)*2-1
-  Y = s/n * np.outer(ustar,ustar) + W
-  u = ustar * innerprod + np.sqrt(sigma) * np.random.normal(size=n)
-  U = AMP_free(Y,s,u,innerprod,iters=10)
-  Unormsq = np.diag(np.dot(np.transpose(U),U))
-  aligns = np.dot(np.transpose(U),ustar) / np.sqrt(Unormsq*n)
+  X = alpha/n * np.outer(ustar,ustar) + W
+  hist_save(np.linalg.eigvalsh(X), '%s_eigvals.png' % prefix)
+  u = ustar * innerprod + sigma * np.random.normal(size=n)
+  U = AMP_free(X,u,innerprod,iters=niters,plot_prefix=prefix)
+  Unormsq = np.diag(np.transpose(U).dot(U))
+  aligns = np.transpose(U).dot(ustar) / np.sqrt(Unormsq*n)
   print(aligns)
 
-'''
+# Gaussian noise
+alpha = 2
+print('Gaussian noise, alpha = 2')
+for i in range(ntrials):
+  prefix='figures/square_gaussian_%d' % i
+  W = np.random.normal(size=(n,n))
+  W = (W+np.transpose(W))/np.sqrt(2*n)
+  test(W,prefix)
 
 # W = ODO' where D is uniform over +/- 1
-print('Noise with eigenvalues +/- 1')
-for i in range(1):
+alpha = 2
+print('Noise with eigenvalues +/- 1, alpha = 2')
+for i in range(ntrials):
+  prefix='figures/square_uniform_%d' % i
   W = np.random.normal(size=(n,n))
   O = np.linalg.qr(W)[0]
   D = np.diag(np.random.binomial(1,0.5,size=n)*2-1)
-  W = np.dot(np.dot(O,D),np.transpose(O))
-  ustar = np.random.binomial(1,0.5,size=n)*2-1
-  Y = s/n * np.outer(ustar,ustar) + W
-  u = ustar * innerprod + np.sqrt(sigma) * np.random.normal(size=n)
-  U = AMP_free(Y,s,u,innerprod,iters=10)
-  Unormsq = np.diag(np.dot(np.transpose(U),U))
-  aligns = np.dot(np.transpose(U),ustar) / np.sqrt(Unormsq*n)
-  print(aligns)
+  W = O.dot(D).dot(np.transpose(O))
+  test(W,prefix)
 
-'''
 # W = ODO' where D is centered and scaled beta
-print('Noise with beta-distributed eigenvalues')
-for i in range(10):
+alpha = 2.5
+print('Noise with beta-distributed eigenvalues, alpha = 2.5')
+for i in range(ntrials):
+  prefix='figures/square_beta_%d' % i
   W = np.random.normal(size=(n,n))
   O = np.linalg.qr(W)[0]
   d = np.random.beta(1,2,size=n)
   d -= np.mean(d)
   d *= np.sqrt(n)/np.linalg.norm(d)
   D = np.diag(d)
-  W = np.dot(np.dot(O,D),np.transpose(O))
-  ustar = np.random.binomial(1,0.5,size=n)*2-1
-  Y = s/n * np.outer(ustar,ustar) + W
-  u = ustar * innerprod + np.sqrt(sigma) * np.random.normal(size=n)
-  U = AMP_free(Y,s,u,innerprod,iters=10)
-  Unormsq = np.diag(np.dot(np.transpose(U),U))
-  aligns = np.dot(np.transpose(U),ustar) / np.sqrt(Unormsq*n)
-  print(aligns)
-'''
+  W = O.dot(D).dot(np.transpose(O))
+  test(W,prefix)
+
