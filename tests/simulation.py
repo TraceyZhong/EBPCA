c254252f1ea70f5abdb73ecdcf587c68e861dc38
Chang <c.su@yale.edu>
2020-10-31 11:02:09 -0400

    modify simulation to work with updated EB-PCA pipeline (e.g. get_pca)

--- tests/simulation.py
+++ tests/simulation.py
@@ -1,20 +1,24 @@
 import numpy as np
-from scipy.linalg import svd
 import os
-from simulation_helper import *
 
 import sys
-sys.path.extend(['/gpfs/ysm/project/zf59/cs/empiricalbayespca/generalAMP'])
-from ebpca.empbayes import NonparEB
-from ebpca.amp import ebamp_gaussian
-from ebpca.pca import signal_solver_gaussian
+sys.path.extend(['../generalAMP'])
+from ebpca.empbayes import NonparEB as NonparEB
+from ebpca.amp import ebamp_gaussian_active as ebamp_gaussian
+from ebpca.preprocessing import normalize_obs
+from ebpca.pca import get_pca, check_residual_spectrum
+from tutorial import get_alignment
+
+# ----------------------
+# Setup for simulation
+# ----------------------
 
 # take named argument from command line
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--prior", type=str, help="enter univariate prior", 
                     default='Uniform', const='Uniform', nargs='?')
-parser.add_argument("--n_data", type=int, help="enter number of independent data to be simulated", 
+parser.add_argument("--n_rep", type=int, help="enter number of independent data to be simulated",
                     default=1, const=1, nargs='?')
 parser.add_argument("--s_star", type=float, help="enter signal strength", 
                     default=1.4, const=1.4, nargs='?')
@@ -23,12 +27,12 @@ parser.add_argument("--iters", type=int, help="enter EB-PCA iterations",
 args = parser.parse_args()
 
 prior = args.prior
-n_data = args.n_data
+n_rep = args.n_rep
 s_star = args.s_star
 iters = args.iters
 
 print('\n Running EB-PCA %i simulations with prior=%s, signal strength=%.1f, iterations=%i \n'\
-      % (n_data, prior, s_star, iters))
+      % (n_rep, prior, s_star, iters))
 
 # create directory to save alignemnt, simulated data and figures
 # where saving data is for reproducible purpose
@@ -37,56 +41,98 @@ if not os.path.exists('output/%s/' % prior):
     os.mkdir('output/%s/alignments' % prior)
     os.mkdir('output/%s/data' % prior)
 
-figprefix = 'figures/simulation/univariate/'
-if not os.path.exists(figprefix):
-   os.mkdir(figprefix)
+fig_prefix = 'figures/simulation/univariate/'
+if not os.path.exists(fig_prefix):
+   os.mkdir(fig_prefix)
 
-n = 1000
-alpha = 1.2
-d = int(n * alpha)
-
-# rank-1 simulation
-
-alignment = []
-
-# set seed and save data for reproducibility
-np.random.seed(1)
-if os.path.exists('output/%s/data/copy_%i.npy' % (prior, n_data - 1)):
+# save data for reproducibility
+if os.path.exists('output/%s/data/copy_%i.npy' % (prior, n_rep - 1)):
     save_data = False
 else:
     save_data = True
 
-for i in range(n_data):
+# save plots only when replication=1
+if n_rep == 1:
+    to_save = True
+    fig_prefix = fig_prefix + '%s/' % prior
+else:
+    to_save = False
+    fig_prefix = '.'
+
+# --------------
+# util functions
+# --------------
+
+# functions for simulating priors and data under the EB-PCA model
+def simulate_prior(prior, n=2000, seed=1):
+    '''
+    simulate 3 distributions with 2nd moment=1
+    Uniform: represents continuous prior
+    Two_points: represents degenerative prior / cluster structure
+    Point_normal: represents sparse prior
+    '''
+    np.random.seed(seed)
+    if prior == 'Uniform':
+        theta = np.random.uniform(-np.sqrt(3), np.sqrt(3), size = n)
+    if prior == 'Two_points':
+        theta = 2 * np.random.binomial(n=1, p=0.5, size=n) - 1
+    if prior == 'Point_normal':
+        point_obs = np.repeat(0, n)
+        assignment = np.random.binomial(n=1, p=0.1, size=n)
+        normal_obs = np.random.normal(loc=0, scale=np.sqrt(10), size=n)
+        theta = point_obs * (1 - assignment) + normal_obs * assignment
+    return theta
+
+def simulate_rank1_model(u, v, s):
+    n = u.shape[0]
+    d = v.shape[0]
+    W = np.random.normal(0, np.sqrt(1/n), n*d).reshape((n, d))
+    A = s * 1 / n * np.outer(u, v) + W
+    return A
+
+# -----------------
+# rank-1 simulation
+# -----------------
+
+# set parameters for simulation
+n = 1000
+alpha = 2
+d = int(n * alpha)
+rank = 1
+alignment = []
+
+# set seed(s) for reproducibility
+np.random.seed(1)
+seeds = [np.random.randint(0, 10000, n_rep) for i in range(2)]
+
+for i in range(n_rep):
     # simulate data based on the chosen prior
-    u_star = simulate_prior(prior, n)
-    v_star = simulate_prior(prior, d)
-    A = simulate_rank1_model(u_star, v_star, s_star)
-    u, s, vh = svd(A, full_matrices=False)
-    u = u[:, 0]
-    vh = vh[0, :]
-    
+    u_star = simulate_prior(prior, n, seed = seeds[0][i])
+    v_star = simulate_prior(prior, d, seed = seed[1][i])
+    Y = simulate_rank1_model(u_star, v_star, s_star)
+    # Normalize the observational matrix.
+    X = normalize_obs(Y, rank)
+    # Prepare the PCA pack.
+    pcapack = get_pca(X, rank)
     # save simulated data
-    # to be consistent across different methods]
+    # to be consistent across different methods
     if save_data:
-        np.save('output/%s/data/copy_%i.npy' % (prior, i), A, allow_pickle=False)
-        np.save('output/%s/data/copy_u_%i.npy' % (prior, i), u, allow_pickle=False)
-        np.save('output/%s/data/copy_vh_%i.npy' % (prior, i), vh, allow_pickle=False)
-    
-    # estimate parameters for AMP
-    init_pars = signal_solver_gaussian(singval=s, n_samples=d, n_features=n)
-    
+        np.save('output/%s/data/copy_%i.npy' % (prior, i), pcapack.X, allow_pickle=False)
+        np.save('output/%s/data/copy_u_%i.npy' % (prior, i), pcapack.U, allow_pickle=False)
+        np.save('output/%s/data/copy_vh_%i.npy' % (prior, i), pcapack.V, allow_pickle=False)
+
     # initiate denoiser
-    udenoiser = NonparEB(to_save=True, to_show=False, fig_prefix=figprefix + '%s/' % prior)
-    vdenoiser = NonparEB(to_save=True, to_show=False, fig_prefix=figprefix + '%s/' % prior)
+    udenoiser = NonparEB(optimizer="Mosek", ftol=1e-3, to_save=to_save, fig_prefix=fig_prefix)
+    vdenoiser = NonparEB(optimizer="Mosek", ftol=1e-3, to_save=to_save, fig_prefix=fig_prefix)
     
     # run AMP
-    U_est, V_est = ebamp_gaussian(A, u, vh, init_pars, iters=iters,
+    U_est, V_est = ebamp_gaussian(pcapack, iters=iters,
                                   udenoiser=udenoiser, vdenoiser=vdenoiser,
-                                  figprefix='s_%.1f' % s_star)
+                                  figprefix='s_%.1f_alpha_%.1f' % (s_star, alpha))
     
     alignment.append([get_alignment(U_est[:, i], u_star) for i in range(U_est.shape[1])])
 
-np.save('output/%s/alignments/n_data_%i.npy' % (prior, n_data), alignment, allow_pickle=False)
+np.save('output/%s/alignments/n_rep_%i.npy' % (prior, n_rep), alignment, allow_pickle=False)
 
 print('\n Simulation finished. \n')
 
