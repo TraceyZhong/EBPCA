430d9957c9038176b8e8b6d915c7dc19f084b8bb
Chang <c.su@yale.edu>
2020-11-15 11:01:25 -0500

    ebmf starts to support nonparametric denoiser

--- ebpca/misc.py
+++ ebpca/misc.py
@@ -27,7 +27,8 @@ from ebpca.empbayes import PointNormalEB, _gaussian_pdf
 # 1. Make it compatible with nonparametric denoiser.
 #    In particular, pay attention to the assumption of prior 2nd moment = 1
 # 2. Implement objective function based convergence
-def ebmf(pcapack, iters = 5, ldenoiser = PointNormalEB(), fdenoiser = PointNormalEB()):
+def ebmf(pcapack, iters = 5, ldenoiser = PointNormalEB(), fdenoiser = PointNormalEB(),
+         update_family = 'point-normal'):
 
     X = pcapack.X
     u, v = pcapack.U, pcapack.V
@@ -39,15 +40,15 @@ def ebmf(pcapack, iters = 5, ldenoiser = PointNormalEB(), fdenoiser = PointNorma
     f_hat = v
 
     # initialize placeholder for l, f update results
-    L = np.reshape(l_hat, (-1,1))
-    F = np.reshape(f_hat, (-1,1))
+    L = l_hat[:,:, np.newaxis] #np.reshape(l_hat, (-1,1))
+    F = f_hat[:,:, np.newaxis] # np.reshape(f_hat, (-1,1))
 
     # use the same scaling as EBMF
     # initialize parameter tau
     tau = n
     # initialize the first update with svd
-    mu = 1
-    sigma_sq = 1 / (np.sum(f_hat**2) * tau)
+    mu = np.diag([1])
+    sigma_sq = np.diag([1 / (np.sum(f_hat**2) * tau)]) # 1 / (np.sum(f_hat**2) * tau)
 
     # first denoise the loadings
     l_hat = X.dot(f_hat) / np.sum(f_hat**2)
@@ -60,33 +61,35 @@ def ebmf(pcapack, iters = 5, ldenoiser = PointNormalEB(), fdenoiser = PointNorma
         ldenoiser.estimate_prior(l_hat, mu, np.sqrt(sigma_sq))
         El = ldenoiser.denoise(l_hat, mu, np.sqrt(sigma_sq))
         Varl = ldenoiser.ddenoise(l_hat, mu, np.sqrt(sigma_sq)) * (sigma_sq / mu)
-        El2 = El**2 + Varl
-        L = np.hstack((L, np.reshape(El,(-1,1))))
+        El2 = El**2 + Varl.reshape(-1,1) #[:,:,0]
+        L = np.dstack((L, np.reshape(El,(-1,1,1))))
         # Evaluate log likelihood
-        [pi, sigma_x] = ldenoiser.get_estimate()
-        print('l prior: %.4f, %.4f' % (pi, sigma_x))
-        KL_l = marginal_lik_F_func([pi, sigma_x],
-                                   l_hat, np.sqrt(sigma_sq), mu, 'point-normal') - \
+        [par1, par2] = ldenoiser.get_estimate()
+        # print('l prior: %.4f, %.4f' % (par1, par2))
+        # par1, par2: pi, sigma_x; par1, par2: pi, Z
+        KL_l = marginal_lik_F_func([par1, par2.reshape(-1)],
+                                   l_hat, np.sqrt(sigma_sq), mu, update_family) - \
                NM_posterior_e_loglik(l_hat, mu, sigma_sq, El, El2)
         # update the estimate of the factor
         f_hat = X.T.dot(El) / np.sum(El2)
-        mu_bar = 1
-        sigma_bar_sq = 1 / (np.sum(El2) * tau)
+        mu_bar = np.diag([1])
+        sigma_bar_sq = np.diag([1 / (np.sum(El2) * tau)])
         fdenoiser.estimate_prior(f_hat, mu_bar, np.sqrt(sigma_bar_sq))
         Ef = fdenoiser.denoise(f_hat, mu_bar, np.sqrt(sigma_bar_sq))
         Varf = fdenoiser.ddenoise(f_hat, mu_bar, np.sqrt(sigma_bar_sq)) * (sigma_bar_sq / mu_bar)
-        Ef2 = Ef**2 + Varf
-        F = np.hstack((F, np.reshape(Ef, (-1,1))))
+        Ef2 = Ef**2 + Varf.reshape(-1,1) # [:,:,0]
+        F = np.dstack((F, np.reshape(Ef, (-1,1,1))))
         # Evaluate log likelihood
-        [pi, sigma_x] = fdenoiser.get_estimate()
-        print('f prior: %.4f, %.4f' % (pi, sigma_x))
-        KL_f = marginal_lik_F_func([pi, sigma_x],
-                                   f_hat, np.sqrt(sigma_bar_sq), mu_bar, 'point-normal') - \
+        [par1, par2] = fdenoiser.get_estimate()
+        # print('f prior: %.4f, %.4f' % (par1, par2))
+        # par1, par2: pi, sigma_x; par1, par2: pi, Z
+        KL_f = marginal_lik_F_func([par1, par2.reshape(-1)],
+                                   f_hat, np.sqrt(sigma_bar_sq), mu_bar, update_family) - \
                NM_posterior_e_loglik(f_hat, mu_bar, sigma_bar_sq, Ef, Ef2)
         # update the estimate of the loading
         l_hat = X.dot(Ef) / np.sum(Ef2)
-        mu = 1
-        sigma_sq = 1 / (np.sum(Ef2) * tau)
+        mu = np.diag([1])
+        sigma_sq = np.diag([1 / (np.sum(Ef2) * tau)])
         # evaluate objective function
         obj_func = get_cond_logl(El, El2, Ef, Ef2, X, tau) + KL_l + KL_f
         obj_funcs.append(obj_func)
--- tests/test_ebmf.py
+++ tests/test_ebmf.py
@@ -3,10 +3,10 @@ import numpy as np
 # Benchmark with implementation from EBMF
 # ----------------------------------------
 import sys
-sys.path.append('/Users/chang/PycharmProjects/generalAMP')
-from ebpca.empbayes import PointNormalEB
+sys.path.append('../../generalAMP')
+from ebpca.empbayes import PointNormalEB, NonparEB
 from ebpca.misc import ebmf
-from scipy.linalg import svd
+from tutorial import get_alignment
 from ebpca.pca import get_pca
 
 # benchmark log likelihood
@@ -17,53 +17,23 @@ from ebpca.pca import get_pca
 A = np.loadtxt('result/simulation/simu_test_ebmf_A.txt')
 u_star = np.loadtxt('result/simulation/simu_test_ebmf_A.txt')
 v_star = np.loadtxt('result/simulation/simu_test_ebmf_A.txt')
-
+n, d = A.shape
 pcapack = get_pca(A, 1)
-ebmf(pcapack) # expected 1st and 2nd values of objective function: 11425635.27, 11425735.36
 
-exit()
+# use parametric Point normal denoiser
+L_est, F_est = ebmf(pcapack, 100) # expected 1st and 2nd values of objective function: 11425635.27, 11425735.36
 
-u0, s, vh0 = svd(A, full_matrices=False)
+# use nonparametric prior
+udenoiser = NonparEB(optimizer="Mosek", ftol=1e-3, to_save=False,
+                     nsupp_ratio=np.sqrt(n)/n)
+vdenoiser = NonparEB(optimizer="Mosek", ftol=1e-3, to_save=False,
+                     nsupp_ratio=np.sqrt(d)/d)
+L_par_est, F_par_est = ebmf(pcapack, 100, ldenoiser=udenoiser, fdenoiser=vdenoiser,
+                            update_family = 'nonparametric')
 
-n = A.shape[0]
-
-# denoiser u0
-EL = u0[:,0]
-EL2 = u0[:,0]**2
-EF = vh0[0,:]
-EF2 = vh0[0,:]**2
-
-elogl = get_cond_logl(EL, EL2, EF, EF2, A, n)
-print(elogl)
-
-# load data
-u0 = np.loadtxt('/Users/chang/Documents/research/EBPCA/npmleAMPv1/result/simulation/test_ebnm_in_R.txt')
-mu = 1
-sigma_y = 4
-
-# also benchmark the estimate of EX and EX^2
-
-# the unit of Point normal denoiser is consistent with ebnm
-
-ests = parametric_mle_point_normal(u0, mu, sigma_y)
-print(ests)
-
-# the units of denoiser is consistent with ebnm
-vdenoiser = PointNormalEB(to_save=True, to_show=False,
-                          pi = ests[0], sigma_x = ests[1])
-EX = vdenoiser.denoise(u0, mu, sigma_y**2)
-VarX = vdenoiser.ddenoise(u0, mu, sigma_y**2) * sigma_y**2 / mu
-EX2 = EX**2 + VarX
-
-print(EX[:6])
-print(EX2[:6])
-
-loglik = point_normal_e_loglik(ests, u0, sigma_y, mu)
-print('Log likeihood: %.4f' % loglik)
-
-print('input data: u0')
-print(u0[:6])
-# NM_posterior_e_loglik
-NM_log = NM_posterior_e_loglik(u0, sigma_y**2, 1, EX, VarX)
-print('NM_posterior_e_loglik')
-print(NM_log)
+print('Parametric denoiser:')
+print([get_alignment(L_est[:, :, j], u_star) for j in range(L_est.shape[2]-10, L_est.shape[2])])
+print('Nonparametric denoiser:')
+print(L_par_est.shape)
+print([get_alignment(L_par_est[:, :, j], u_star) for j in
+       range(L_par_est.shape[2] - 10, L_par_est.shape[2])])
