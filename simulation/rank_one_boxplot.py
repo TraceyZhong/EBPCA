7e5f4192f2ea876ba535b6c24306a5608d7620bd
Chang <c.su@yale.edu>
2020-11-18 17:51:53 -0500

    chore: organize codes for rank one boxplots and hist

--- simulation/rank_one_boxplot.py
+++ simulation/rank_one_boxplot.py
@@ -19,8 +19,18 @@ PC = args.PC
 # boxplot
 # ----------------------------------------------
 
-def alignment_boxplots(pca, bayesamp, ebpca, ebmf, spca, ticks):
-
+def alignment_boxplots(res, ticks):
+    n = len(res)
+    if n == 5:
+        pca, bayesamp, ebpca, ebmf, spca = res
+        plot_seps = [-1.2, -0.6, 0.0, 0.6, 1.2]
+        bp_width = 0.45
+    else:
+        pca, bayesamp, ebpca, ebmf = res
+        n = 4
+        plot_seps = [-1.2, -0.4, 0.4, 1.2]
+        bp_width = 0.45
+    bp_colors = ['tab:grey', 'tab:orange', 'tab:red', 'tab:blue', 'tab:green']
     # reference: https://stackoverflow.com/questions/16592222/matplotlib-group-boxplots 2nd answer
     f, ax = plt.subplots(figsize=(8, 6))
     def set_box_color(bp, color):
@@ -29,30 +39,34 @@ def alignment_boxplots(pca, bayesamp, ebpca, ebmf, spca, ticks):
         plt.setp(bp['caps'], color=color)
         plt.setp(bp['medians'], color=color)
 
-    bp1 = ax.boxplot(pca, positions=np.array(range(len(pca))) * 5.0 - 1.2, sym='', widths=0.45)
-    bp2 = ax.boxplot(bayesamp, positions=np.array(range(len(ebpca))) * 5.0 - 0.6, sym='', widths=0.45)
-    bp3 = ax.boxplot(ebpca, positions=np.array(range(len(ebmf))) * 5.0 + 0.0, sym='', widths=0.45)
-    bp4 = ax.boxplot(ebmf, positions=np.array(range(len(spca))) * 5.0 + 0.6, sym='', widths=0.45)
-    bp5 = ax.boxplot(spca, positions=np.array(range(len(spca))) * 5.0 + 1.2, sym='', widths=0.45)
+    bp1 = ax.boxplot(pca, positions=np.array(range(len(pca))) * float(n) + plot_seps[0], sym='', widths=bp_width)
+    bp2 = ax.boxplot(bayesamp, positions=np.array(range(len(bayesamp))) * float(n) + plot_seps[1], sym='', widths=bp_width)
+    bp3 = ax.boxplot(ebpca, positions=np.array(range(len(ebpca))) * float(n) + plot_seps[2], sym='', widths=bp_width)
+    bp4 = ax.boxplot(ebmf, positions=np.array(range(len(ebmf))) * float(n) + plot_seps[3], sym='', widths=bp_width)
+    if n == 5:
+        bp5 = ax.boxplot(spca, positions=np.array(range(len(spca))) * float(n) + plot_seps[4], sym='', widths=bp_width)
 
-    set_box_color(bp1, '#636363')
-    set_box_color(bp2, '#fc9272')
-    set_box_color(bp3, '#de2d26')
-    set_box_color(bp4, '#3182bd')
-    set_box_color(bp5, '#31a354')  # colors are from http://colorbrewer2.org/
+    # https://matplotlib.org/3.1.0/gallery/color/named_colors.html
+    set_box_color(bp1, bp_colors[0])
+    set_box_color(bp2, bp_colors[1])
+    set_box_color(bp3, bp_colors[2])
+    set_box_color(bp4, bp_colors[3])
+    if n == 5:
+        set_box_color(bp5, bp_colors[4])  # colors are from http://colorbrewer2.org/
 
     # draw temporary red and blue lines and use them to create a legend
-    ax.plot([], c='#636363', label='PCA')
-    ax.plot([], c='#fc9272', label='BayesAMP')
-    ax.plot([], c='#de2d26', label='EB-PCA')
-    ax.plot([], c='#3182bd', label='EBMF')
-    ax.plot([], c='#31a354', label='sPCA')
+    ax.plot([], c=bp_colors[0], label='PCA')
+    ax.plot([], c=bp_colors[1], label='BayesAMP')
+    ax.plot([], c=bp_colors[2], label='EB-PCA')
+    ax.plot([], c=bp_colors[3], label='EBMF')
+    if n == 5:
+        ax.plot([], c=bp_colors[4], label='sPCA')
 
     ax.legend(loc='lower right')
-    plt.xticks(range(0, len(ticks) * 5, 5), ticks)
+    plt.xticks(range(0, len(ticks) * n, n), ticks)
     ax.set_xlabel("signal strength")
     ax.set_ylabel('alignment with ground truth')
-    ax.set_xlim(-2, len(ticks) * 5)
+    ax.set_xlim(-2, len(ticks) * n)
     # plt.ylim(0, 1)
     # plt.title('Sqaured alignment, {} \n n={}, sparsity={}, alpha={}, {} runs'.format(vector, n, sparsity, gamma, nruns))
     return ax
@@ -63,37 +77,50 @@ def alignment_boxplots(pca, bayesamp, ebpca, ebmf, spca, ticks):
 # os.chdir('/Users/chang/PycharmProjects/generalAMP/simulation/')
 
 # load alignments from 50 replications
-def load_alignments(prior, method, ind=-1, PC='left', s_list=[1.3, 1.8, 3.0]):
+def load_alignments(prior, method, ind=-1, PC='left', rm_na =True, s_list=[1.3, 1.8, 3.0]):
     n_par = len(s_list)
     align_dir = 'output/univariate/%s/alignments' % prior
     if PC=='left':
-        aligns = [(np.load('%s/%s_u_s_%.1f_n_rep_50.npy' % (align_dir, method, s))[:, ind]).tolist() for s in s_list]
+        aligns = [(np.load('%s/%s_u_s_%.1f_n_rep_50.npy' % (align_dir, method, s))[:, ind]) for s in s_list]
     else:
-        aligns = [(np.load('%s/%s_v_s_%.1f_n_rep_50.npy' % (align_dir, method, s))[:, ind]).tolist() for s in s_list]
-    # remove nan values:
-    print('Number of NA alignments for %s, %s' % (method, prior), [np.sum(np.isnan(aligns[i])) for i in range(n_par)])
-    aligns = [np.array(aligns[i])[~np.isnan(aligns[i])].tolist() for i in range(n_par)]
+        aligns = [(np.load('%s/%s_v_s_%.1f_n_rep_50.npy' % (align_dir, method, s))[:, ind]) for s in s_list]
+
+    if rm_na:
+        # remove nan values:
+        print('Number of NA alignments for %s, %s' % (method, prior), [np.sum(np.isnan(aligns[i])) for i in range(n_par)])
+        aligns = [np.array(aligns[i])[~np.isnan(aligns[i])] for i in range(n_par)]
+    else:
+        aligns = np.array(aligns)
     return aligns
 
+def group_alignments(prior, PC, rm_na = True):
+    pca = load_alignments(prior, 'EB-PCA', 0, PC, rm_na)
+    bayesamp = load_alignments(prior, 'BayesAMP', -1, PC, rm_na)
+    ebpca = load_alignments(prior, 'EB-PCA', -1, PC, rm_na)
+    ebmf = load_alignments(prior, 'EBMF', -1, PC, rm_na)
+    if prior == 'Point_normal':
+        # TODO: load spca
+        spca = pca
+        res = [pca, bayesamp, ebpca, ebmf, spca]
+    else:
+        res = [pca, bayesamp, ebpca, ebmf]
+    return res
+
 # prior = 'Point_normal' # 'Two_points' # 'Uniform'
-def make_comp_plot(prior, PC, s_list = [1.3, 1.8, 3.0]):
-    pca = load_alignments(prior, 'EB-PCA', 0, PC)
-    bayesamp = load_alignments(prior, 'BayesAMP', -1, PC)
-    ebpca = load_alignments(prior, 'EB-PCA', -1, PC)
-    ebmf = load_alignments(prior, 'EBMF', -1, PC)
-    alignment_boxplots(pca, bayesamp, ebpca, ebmf, ebpca, s_list)
+def make_comp_plot(res, s_list = [1.3, 1.8, 3.0]):
+    alignment_boxplots(res, s_list)
     plt.title('%s, %s PC, method comparison' % (prior, PC))
     plt.savefig('figures/univariate/Figure1/%s_%sPC_method_comp_boxplots.png' % (prior, PC))
+    plt.close()
 
-# ----------------------------------------------
-# Figure 3:
-# alignment comparison across methods
-# boxplot
-# ----------------------------------------------
 
 
 
 if __name__ == '__main__':
-    for prior in ['Point_normal', 'Two_points', 'Uniform']:
-        for PC in ['left', 'right']:
-            make_comp_plot(prior, PC)
+    # Figure 1
+    f1= True
+    if f1:
+        for prior in ['Point_normal', 'Two_points', 'Uniform']:
+            for PC in ['left', 'right']:
+                res = group_alignments(prior, PC)
+                make_comp_plot(res)
