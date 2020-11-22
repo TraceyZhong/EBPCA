378c166f66998e8e4ed62d5da67fc90e1374f96c
Chang Su <c.su@yale.edu>
2020-11-22 00:31:41 -0500

    chore: update batch scripts

--- simulation/test_minimum_s.sh
+++ simulation/test_minimum_s.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 s=$1
-echo '\n EB-PCA'
-echo '\n Point Normal \n'
-python rank_one.py --prior=Point_normal --iters=20 --s_star=$s
-echo '\n Two points \n'
-python rank_one.py --prior=Two_points --iters=20 --s_star=$s
-echo '\n Uniform \n'
-python rank_one.py --prior=Uniform --iters=20 --s_star=$s
-echo '\n EBMF\n'
-echo '\n Point Normal \n'
-python rank_one.py --prior=Point_normal --iters=20 --method=EBMF --s_star=$s
-echo '\n Two points \n'
-python rank_one.py --prior=Two_points --iters=20 --method=EBMF --s_star=$s
-echo '\n Uniform \n'
-python rank_one.py --prior=Uniform --iters=20 --method=EBMF --s_star=$s
+iters=$2
+echo "s=$s"
+echo ""
+echo "EB-PCA"
+echo ""
+echo "Point Normal"
+echo ""
+python rank_one.py --prior=Point_normal --iters=$iters --s_star=$s
+echo ""
+echo "Two points"
+echo ""
+python rank_one.py --prior=Two_points --iters=$iters --s_star=$s
+echo ""
+echo "Uniform"
+echo ""
+python rank_one.py --prior=Uniform --iters=$iters --s_star=$s
+echo ""
+echo "EBMF"
+echo ""
+echo "Point Normal"
+echo ""
+python rank_one.py --prior=Point_normal --iters=$iters --method=EBMF --s_star=$s
+echo ""
+echo "Two points"
+echo ""
+python rank_one.py --prior=Two_points --iters=$iters --method=EBMF --s_star=$s
+echo ""
+echo "Uniform"
+echo ""
+python rank_one.py --prior=Uniform --iters=$iters --method=EBMF --s_star=$s
