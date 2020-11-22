31d375d47bdbe5b6445796289dc1ea27cbf241d5
Xinyi Zhong <zhongxy14@gmail.com>
2020-11-22 18:24:44 -0500

    state evolution with bugs

--- ebpca/__init__.py
+++ ebpca/__init__.py
@@ -7,8 +7,4 @@ if not os.path.exists("figures"):
 from . import amp
 from . import pca
 from . import empbayes
-<<<<<<< HEAD
 from . import preprocessing
-=======
-from . import preprocessing
->>>>>>> 74e4318... reset the algorithm as the paper
