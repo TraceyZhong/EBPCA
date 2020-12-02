fb9fbf32fdc10041d03e3c0fdd992b0c3935ac55
Chang <c.su@yale.edu>
2020-12-02 09:45:09 -0500

    new error evaluation script for real data

--- examples/evaluation.py
+++ examples/evaluation.py
@@ -5,36 +5,47 @@ sys.path.extend(['../../generalAMP'])
 from simulation.helpers import get_space_distance
 import numpy as np
 
+subset_sizes = {'UKBB': [500, 2000, 10000], '1000G': [100, 1000, 10000]}
+
 for data_name in ['UKBB', '1000G']:
     V_star = np.load('results/%s/ground_truth_PC.npy' % data_name)
     d = V_star.shape[1]
     PCA_error = np.empty((50, 1+d))
     EBPCA_error = np.empty((50, 1+d))
-    for n_copy in range(1, 51, 1):
-        PC_est = np.load('results/%s/PC_estimates_iters_5_n_copy_%s.npy' % (data_name, n_copy))
-        PCA = PC_est[:, :, 0]
-        EBPCA = PC_est[:, :, 5]
-        # PCA_error
-        PCA_error_tmp = [get_space_distance(PCA[:, [j]], V_star[:, [j]]) for j in range(d)]
-        PCA_error_tmp.append(get_space_distance(PCA, V_star))
-        PCA_error[n_copy - 1, :] = PCA_error_tmp
-        # EBPCA error
-        EBPCA_error_tmp = [get_space_distance(EBPCA[:, [j]], V_star[:, [j]]) for j in range(d)]
-        EBPCA_error_tmp.append(get_space_distance(EBPCA, V_star))
-        EBPCA_error[n_copy - 1, :] = EBPCA_error_tmp
-    np.save('results/%s/PCA_error_summary_n_rep_50.npy' % data_name, PCA_error)
-    np.save('results/%s/EBPCA_error_summary_n_rep_50.npy' % data_name, EBPCA_error)
+    for subset_size in subset_sizes[data_name]:
+        for n_copy in range(1, 51, 1):
+            if subset_size > 500 and subset_size < 2500:
+                PC_est = np.load('results/%s/PC_estimates_iters_5_n_copy_%s.npy' % (data_name, n_copy))
+            else:
+                PC_est = np.load('results/%s/PC_estimates_iters_5_size_%i_n_copy_%s.npy' % \
+                                 (data_name, subset_size, n_copy))
+            PCA = PC_est[:, :, 0]
+            EBPCA = PC_est[:, :, 5]
+            # PCA_error
+            PCA_error_tmp = [get_space_distance(PCA[:, [j]], V_star[:, [j]]) for j in range(d)]
+            PCA_error_tmp.append(get_space_distance(PCA, V_star))
+            PCA_error[n_copy - 1, :] = PCA_error_tmp
+            # EBPCA error
+            EBPCA_error_tmp = [get_space_distance(EBPCA[:, [j]], V_star[:, [j]]) for j in range(d)]
+            EBPCA_error_tmp.append(get_space_distance(EBPCA, V_star))
+            EBPCA_error[n_copy - 1, :] = EBPCA_error_tmp
+        np.save('results/%s/PCA_error_summary_size_%i_n_rep_50.npy' % (data_name, subset_size), PCA_error)
+        np.save('results/%s/EBPCA_error_summary_size_%i_n_rep_50.npy' % (data_name, subset_size), EBPCA_error)
 
 # summarize mean and std
 for data_name in ['UKBB', '1000G']:
-    print('\n\n %s \n\n' % data_name)
-    PCA_error = np.load('results/%s/PCA_error_summary_n_rep_50.npy' % data_name)
-    EBPCA_error = np.load('results/%s/EBPCA_error_summary_n_rep_50.npy' % data_name)
-    print('\n ############ %s ############ \n' % data_name)
-    print('from left to right: PC 1-%i, Joint' % (PCA_error.shape[1] - 1))
-    print('PCA errors: ')
-    print('\t mean ', np.mean(PCA_error, axis=0))
-    print('\t sd ', np.std(PCA_error, axis=0))
-    print('EBPCA errors:')
-    print('\t mean ', np.mean(EBPCA_error, axis=0))
-    print('\t sd ', np.std(EBPCA_error, axis=0))
+    print('\n############')
+    print('%s' % data_name)
+    print('############\n')
+    for subset_size in subset_sizes[data_name]:
+        # print('\n %s \n' % subset_size)
+        PCA_error = np.load('results/%s/PCA_error_summary_size_%i_n_rep_50.npy' % (data_name, subset_size))
+        EBPCA_error = np.load('results/%s/EBPCA_error_summary_size_%i_n_rep_50.npy' % (data_name, subset_size))
+        print('\n ############ %s ############ \n' % subset_size)
+        print('from left to right: PC 1-%i, Joint' % (PCA_error.shape[1] - 1))
+        print('PCA errors: ')
+        print('\t mean ', np.mean(PCA_error, axis=0))
+        print('\t sd ', np.std(PCA_error, axis=0))
+        print('EBPCA errors:')
+        print('\t mean ', np.mean(EBPCA_error, axis=0))
+        print('\t sd ', np.std(EBPCA_error, axis=0))
