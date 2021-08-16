69eda2bc26bd9ae53455ca21c0532ed838c47547
Chang Su <c.su@yale.edu>
2021-08-16 14:41:28 -0400

    add cross iterations eval for EBPCA as well

--- tests/MFVB_evaluation.py
+++ tests/MFVB_evaluation.py
@@ -1,4 +1,5 @@
-# provide quantitative assessment for EB-PCA on 1000G and UKBB
+# summarize estimation errors for EB-PCA and MF-VB
+# across iterations on 1000G
 
 import sys
 sys.path.extend(['../../generalAMP'])
@@ -13,15 +14,32 @@ os.chdir('../examples')
 
 V_star = np.load('results/%s/ground_truth_PC.npy' % data_name)
 d = V_star.shape[1]
+
+# EB-PCA
+for subset_size in subset_sizes:
+    errors_by_iters = []
+    for n_copy in range(1, 51, 1):
+        PC_est = np.load('results/%s/PC_estimates_iters_5_size_%i_n_copy_%i.npy' % \
+                                 (data_name, subset_size, n_copy))
+        # joint estimation errors across iterations
+        errors_by_iters.append([get_space_distance(PC_est[:, :, i], V_star) for i in range(6)])
+        np.save('results/%s/EBPCA_error_across_iterations_summary_size_%i_n_rep_50.npy' % (data_name, subset_size), errors_by_iters)
+
+# MF-VB
 for RMT_ini in [True, False]:
-    error = np.empty((50, 1+d))
     for subset_size in subset_sizes:
+        error = np.empty((50, 1+d))
+        errors_by_iters = []
         for n_copy in range(1, 51, 1):
             # if subset_size > 500 and subset_size < 2500:
             #     PC_est = np.load('results/%s/PC_estimates_iters_5_n_copy_%s.npy' % (data_name, n_copy))
             # else:
             PC_est = np.load('results/%s/PC_estimates_iters_5_size_%i_n_copy_%s_MF-VB_RMT_%s.npy' % \
                                  (data_name, subset_size, n_copy, RMT_ini))
+            # joint estimation errors across iterations
+            errors_by_iters.append([get_space_distance(PC_est[:, :, i], V_star) for i in range(6)])
+            np.save('results/%s/MF-VB_RMT_%s_error_across_iterations_summary_size_%i_n_rep_50.npy' % (data_name, RMT_ini, subset_size), errors_by_iters)
+            # estimation errors by PCs at the last iteration
             MFVB_est = PC_est[:, :, -1]
             # MF-VB error
             error_tmp = [get_space_distance(MFVB_est[:, [j]], V_star[:, [j]]) for j in range(d)]
